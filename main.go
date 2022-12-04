ad64e38827948b40f855f5c6436c51c905162fbd
Richard Tang <Richard_L_Tang@mckinsey.com>
2022-12-04 23:19:00 +0700

    feat: json response & status code

--- main.go
+++ main.go
@@ -8,58 +8,79 @@ import (
 	"net/http"
 )
 
+type Note struct {
+	Id int
+	Content string
+}
+
+func ReadData() ([]Note, error) {
+	data, err := ioutil.ReadFile("data.json")
+	if err != nil {
+		return []Note{}, err
+	}
+	var todos []Note
+	if err = json.Unmarshal(data, &todos); err != nil {
+		return []Note{}, err
+	}
+	return todos, nil
+}
+
+func Response(w http.ResponseWriter, responseBody []byte, contentType string, statusCode int) {
+	w.Header().Set("Content-Type", contentType)
+	w.WriteHeader(statusCode)
+	w.Write(responseBody)
+}
+
+func ReadNoteFromRequest(r *http.Request) (Note, error) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		return Note{}, err
+	}
+	var jsonData Note
+	if err = json.Unmarshal(data, &jsonData); err != nil {
+		return Note{}, err
+	}
+	return jsonData, nil
+}
+
+func WriteToFile(content []Note) error {
+	output, err := json.Marshal(content)
+	if err != nil {
+		return err
+	}
+	if err = ioutil.WriteFile("data.json", output, 0666); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
-			// read from file
-			data, err := ioutil.ReadFile("data.json")
+			data, err := ReadData()
 			if err != nil {
-				// return error in response
 				fmt.Fprintf(w, "Failed to get data: %s", err.Error())
-				return
 			}
-
-			// set response
-			w.Write(data)
+			response, _ := json.Marshal(data)
+			Response(w, response, "application/json", http.StatusOK)
 		case "POST":
-			// read POST request body
-			data, err := io.ReadAll(r.Body)
+			requestBody, err := ReadNoteFromRequest(r)
 			if err != nil {
 				fmt.Fprintf(w, "Failed to get data: %s", err.Error())
 				return
 			}
-
-			// read existing data from file
-			existing_data, err := ioutil.ReadFile("data.json")
+			todos, err := ReadData()
 			if err != nil {
 				fmt.Fprintf(w, "Failed to get existing data: %s", err.Error())
 			}
-
-			// parse existing string data to json
-			var todos []string
-			if err = json.Unmarshal(existing_data, &todos); err != nil {
-				fmt.Fprintf(w, "Failed to parse existing data: %s", err.Error())
-				return
-			}
-
-			// add new data
-			todos = append(todos, string(data))
-			// convert parsed data to string
-			output, err := json.Marshal(todos)
-			if err != nil {
-				fmt.Fprintf(w, "Failed to convert to json: %s", err.Error())
-			}
-
-			// write to file
-			if err = ioutil.WriteFile("data.json", output, 0666); err != nil {
+			todos = append(todos, requestBody)
+			if err = WriteToFile(todos); err != nil {
 				fmt.Fprintf(w, "Failed to write to data.json: %s", err.Error())
 			}
-
-			// set response
-			w.Write([]byte("Successfully wrote data"))
+			Response(w, []byte("Successfully wrote data"), "text", http.StatusCreated)
 		default:
-			fmt.Fprintf(w, "Method %s not supported", r.Method)
+			Response(w, []byte(fmt.Sprintf("Method %s not supported", r.Method)), "text", http.StatusMethodNotAllowed)
 		}
 	})
 
